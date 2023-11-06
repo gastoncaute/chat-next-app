79a72016b774e4cc2ae97e1a86882c29b8b26e59
gastoncaute <gastoncaute@gmail.com>
2023-11-06 12:39:50 -0300

    a

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,16 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import ChatPage from './chat'
-import Link from 'next/link'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import ChatPage from "./chat";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <Link href='http://localhost:3000/chat'>Iniciat Chat</Link>
+      <Link href="/chat">Iniciat Chat</Link>
     </main>
-  )
+  );
 }
