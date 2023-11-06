42f13b3d7a406ff2b321c41c353a078068913627
gastoncaute <gastoncaute@gmail.com>
2023-11-06 12:43:14 -0300

    a

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <ChatPage />
+      <Link href="/chat">Iniciat Chat</Link>
     </main>
   );
 }
